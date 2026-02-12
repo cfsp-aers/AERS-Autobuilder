94ad1ca4c32d798f565cd3195810b696ee775436
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-12 12:02:07 +1100

    regular update

--- tests/test_js.js
+++ tests/test_js.js
@@ -1,16 +1,16 @@
 const _ = require("lodash");
-const { load } = require("../../../src/main/utils/load.js");
-const { app_dir, user_files } = require("../../../src/main/constants.js");
+const { load } = require("../main/utils/load.js");
+const { app_dir, user_files } = require("../main/constants.js");
 const aers = load(app_dir, "main/utils/aers utilities.js");
 const { setComponents } = load(app_dir, "main/systems/setComponents.js");
 
 function test_function(data) {
-   _.forEach(data, (item) => {
+    _.forEach(data, (item) => {
         _.forIn(item, (value, key) => {
-        item[key] = `${value}_modified`;
+            item[key] = `${value}_modified`;
+        });
     });
-   });
-    
+
     return data;
 }
 
