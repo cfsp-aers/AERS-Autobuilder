bbb13944ba3d28a7c8b029bbaf794bc51d5f9a6e
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-16 14:59:17 +1100

    _

--- src/main/processing/structureEDM.js
+++ src/main/processing/structureEDM.js
@@ -261,7 +261,7 @@ function start_new_block_container(child, prev) {
     if (start_new_group_column(child, prev)) return true;
     if (child.name != prev.name) return true;
     if (child.group_size != prev.group_size) return true;
-    if (child.row_index > 1) return false;
+    if (child.row_index == 1) return true;
     if (child.background != prev.background) return true;
     //if (child.row_index == 1) return true;
     return false;
