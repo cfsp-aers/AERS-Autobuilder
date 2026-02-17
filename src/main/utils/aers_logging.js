000d245c61d6125658e1fea4081cb6aab3e8261b
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-17 15:14:20 +1100

    _

--- src/main/utils/aers_logging.js
+++ src/main/utils/aers_logging.js
@@ -2,17 +2,13 @@ function logToRenderer(window, message) {
     window.webContents.send("main-log", message);
 }
 
-const originalLog = console.log;
-
 const log =
     (window) =>
     (...args) => {
-        originalLog(...args);
-        logToRenderer(window, args.join(" "));
+        if (window != undefined) logToRenderer(window, args.join(" "));
     };
 
 module.exports = {
     log: log,
-    logToRenderer: logToRenderer,
-    originalLog: originalLog
+    logToRenderer: logToRenderer
 };
