540e231d671b4a2f3360c3a98975809e07757bb8
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-18 16:01:31 +1100

    _

--- src/main/processing/applyModifications.js
+++ src/main/processing/applyModifications.js
@@ -36,7 +36,8 @@ function applyModifications(db, func, max_loops) {
                                 overwrite === true ? (target_module.locked_settings = value) : (target_module.locked_settings ??= value);
                             } else {
                                 if (key.includes("[")) {
-                                    console.log("c_name_list", key);
+                                    const component_name_list = JSON.parse(key);
+                                    console.log(component_name_list);
                                 } else {
                                     const i_list = key.includes("/") ? formatIndices(key.split("/")[1]) : ["all"];
                                     const component_name = key.includes("/") ? key.split("/")[0] : key;
