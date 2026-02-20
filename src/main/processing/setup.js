c0f333ed7ad3aeb095ea26bafe9ba54bf4a76b25
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-20 15:14:19 +1100

    _

--- src/main/processing/setup.js
+++ src/main/processing/setup.js
@@ -90,6 +90,7 @@ function setBasicProperties(object, header = {}) {
             default:
                 break;
         }
+        object.dynamicContent ??= object.inVersions;
     }
     //object.brand = object.brand ? object.brand.toLowerCase() : setBrand(object.brand, header.brand);
     //object.parent_brand = object.parent_brand ? object.parent_brand.toLowerCase() : setBrand(object.brand, header.brand, "parent");
