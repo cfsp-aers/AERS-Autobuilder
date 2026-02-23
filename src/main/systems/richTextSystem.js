68d3180245c20fe12bcdea67d1ddcfa005dd91f6
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-24 10:47:08 +1100

    _

--- src/main/systems/richTextSystem.js
+++ src/main/systems/richTextSystem.js
@@ -79,7 +79,7 @@ function formatRichText(item, text) {
 
 function insertRichText(c) {
     if (_.isEmpty(c.text_segments)) {
-        if (c.content) {
+        if (typeof c.content == "string") {
             if (c.content.includes("\\")) {
                 c.content = c.content.replaceAll(/\\(.)/g, "$1");
             } else if (c.content.includes("^")) {
