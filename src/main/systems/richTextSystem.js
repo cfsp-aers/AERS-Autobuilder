f8b6c3258dda61a68142e0a801c2903d41681913
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-24 10:45:57 +1100

    _

--- src/main/systems/richTextSystem.js
+++ src/main/systems/richTextSystem.js
@@ -79,11 +79,7 @@ function formatRichText(item, text) {
 
 function insertRichText(c) {
     if (_.isEmpty(c.text_segments)) {
-        if (c.content.includes("\\")) {
-            c.content = c.content.replaceAll(/\\(.)/g, "$1");
-        } else if (c.content.includes("^")) {
-            c.content = c.content.replaceAll(/\^(.)/g, '<sup style="line-height: 0px !important;">$1</sup>');
-        }
+        console.log(c);
         return c;
     } else if (c.text_segments.length == 0) return c;
     else {
