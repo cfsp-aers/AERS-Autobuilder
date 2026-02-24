86c5c29f847e6689b3783cbc1fd67d9857d479a6
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-24 13:49:31 +1100

    _

--- src/main/processing/structureEDM.js
+++ src/main/processing/structureEDM.js
@@ -263,7 +263,7 @@ function start_new_block_row(child, prev) {
 function start_new_block_container(child, prev) {
     if (start_new_structure(child, prev)) return true;
     if (child.name != prev.name) return true;
-    if (child.group_size != prev.group_size) return true;
+    //if (child.group_size != prev.group_size) return true;
     if (child.background != prev.background) return true;
     if (child.dynamicContent?.split("/")[1] != prev.dynamicContent?.split("/")[1]) return true;
     //if (child.row_index == 1) return true;
