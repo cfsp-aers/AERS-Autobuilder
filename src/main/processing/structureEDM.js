5e0aea3117c761accb6527e71dba8ddb61bda338
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-24 14:26:52 +1100

    _

--- src/main/processing/structureEDM.js
+++ src/main/processing/structureEDM.js
@@ -137,8 +137,9 @@ function structureEDM(arr, children = {}) {
     // blockContainer
     result = result.reduce((acc, item, index) => {
         console.log("block row length : ", item.children.length);
-        item.children = item.children.map((block_col) => {
+        item.children = item.children.map((block_col, index) => {
             block_col.width = `${_.round(100 / _.toInteger(item.children.length), 4)}%`;
+            block_col.row_index = index;
             return block_col;
         });
         const child_index = _.findIndex(arr, (m) => m.uuid == item.uuid);
