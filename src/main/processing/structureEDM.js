639155a999f37b5fed4d4c02eaf076a73bb13e66
cfsp-aers <charles.pollaers@gxltd.com.au>
2026-02-24 14:13:36 +1100

    _

--- src/main/processing/structureEDM.js
+++ src/main/processing/structureEDM.js
@@ -68,7 +68,7 @@ function structureEDM(arr, children = {}) {
         // conditions for grouping
         if (child.type == "fragment" || child.fragment == true) acc.push(item);
         else if (prev.type == "fragment" || prev.fragment == true) acc.push(grouped_item);
-        else if (start_new_module_column(child, prev) === true) acc.push(grouped_item);
+        else if (start_new_module_column(child, prev, index) === true) acc.push(grouped_item);
         else _.last(acc).children.push(item);
         return acc;
     }, []);
@@ -89,7 +89,7 @@ function structureEDM(arr, children = {}) {
         };
         if (child.type == "fragment" || child.fragment == true) acc.push(item);
         else if (prev.type == "fragment" || prev.fragment == true) acc.push(grouped_item);
-        else if (start_new_module_container(child, prev) === true) acc.push(grouped_item);
+        else if (start_new_module_container(child, prev, index) === true) acc.push(grouped_item);
         else _.last(acc).children.push(item);
         return acc;
     }, []);
@@ -110,7 +110,7 @@ function structureEDM(arr, children = {}) {
         };
         if (child.type == "fragment" || child.fragment == true) acc.push(item);
         else if (prev.type == "fragment" || prev.fragment == true) acc.push(grouped_item);
-        else if (start_new_block_column(child, prev) === true) acc.push(grouped_item);
+        else if (start_new_block_column(child, prev, index) === true) acc.push(grouped_item);
         else _.last(acc).children.push(item);
         return acc;
     }, []);
@@ -129,7 +129,7 @@ function structureEDM(arr, children = {}) {
         };
         if (child.type == "fragment" || child.fragment == true) acc.push(item);
         else if (prev.type == "fragment" || prev.fragment == true) acc.push(grouped_item);
-        else if (start_new_block_row(child, prev) === true) acc.push(grouped_item);
+        else if (start_new_block_row(child, prev, index) === true) acc.push(grouped_item);
         else _.last(acc).children.push(item);
         return acc;
     }, []);
@@ -161,7 +161,7 @@ function structureEDM(arr, children = {}) {
         else if (prev.hide_transition === true) grouped_item.transition = false;
         if (child.type == "fragment" || child.fragment == true) acc.push(item);
         else if (prev.type == "fragment" || prev.fragment == true) acc.push(grouped_item);
-        else if (start_new_block_container(child, prev) === true) acc.push(grouped_item);
+        else if (start_new_block_container(child, prev, index) === true) acc.push(grouped_item);
         else _.last(acc).children.push(item);
         return acc;
     }, []);
@@ -181,7 +181,7 @@ function structureEDM(arr, children = {}) {
         };
         if (child.type == "fragment" || child.fragment == true) acc.push(item);
         else if (prev.type == "fragment" || prev.fragment == true) acc.push(grouped_item);
-        else if (start_new_group_column(child, prev) === true) acc.push(grouped_item);
+        else if (start_new_group_column(child, prev, index) === true) acc.push(grouped_item);
         else _.last(acc).children.push(item);
         return acc;
     }, []);
@@ -199,7 +199,7 @@ function structureEDM(arr, children = {}) {
         };
         if (child.type == "fragment" || child.fragment == true) acc.push(item);
         else if (prev.type == "fragment" || prev.fragment == true) acc.push(grouped_item);
-        else if (start_new_group_row(child, prev) === true) acc.push(grouped_item);
+        else if (start_new_group_row(child, prev, index) === true) acc.push(grouped_item);
         else _.last(acc).children.push(item);
         return acc;
     }, []);
@@ -218,7 +218,7 @@ function structureEDM(arr, children = {}) {
         };
         if (child.type == "fragment" || child.fragment == true) acc.push(item);
         else if (prev.type == "fragment" || prev.fragment == true) acc.push(grouped_item);
-        else if (start_new_group_container(child, prev) === true) acc.push(grouped_item);
+        else if (start_new_group_container(child, prev, index) === true) acc.push(grouped_item);
         else _.last(acc).children.push(item);
         return acc;
     }, []);
@@ -240,7 +240,7 @@ function structureEDM(arr, children = {}) {
         };
         if (child.type == "fragment" || child.fragment == true) acc.push(item);
         else if (prev.type == "fragment" || prev.fragment == true) acc.push(grouped_item);
-        else if (start_new_structure(child, prev) === true) acc.push(grouped_item);
+        else if (start_new_structure(child, prev, index) === true) acc.push(grouped_item);
         else _.last(acc).children.push(item);
         return acc;
     }, []);
@@ -249,24 +249,25 @@ function structureEDM(arr, children = {}) {
 
     //aers.updateEntityStore(es);
 }
-function start_new_module_column(child, prev) {
+function start_new_module_column(child, prev, index) {
     return true;
 }
-function start_new_module_container(child, prev) {
-    if (start_new_block_column(child, prev)) return true;
+function start_new_module_container(child, prev, index) {
+    if (start_new_block_column(child, prev, index)) return true;
     return true;
 }
-function start_new_block_column(child, prev) {
-    if (start_new_block_row(child, prev)) return true;
+function start_new_block_column(child, prev, index) {
+    if (start_new_block_row(child, prev, index)) return true;
     return true;
 }
-function start_new_block_row(child, prev) {
-    if (start_new_block_container(child, prev)) return true;
+function start_new_block_row(child, prev, index) {
+    if (start_new_block_container(child, prev, index)) return true;
+    if (child.max_siblings >= index) return true;
     // if (child.row_index == 1) return true;
     return false;
 }
-function start_new_block_container(child, prev) {
-    if (start_new_structure(child, prev)) return true;
+function start_new_block_container(child, prev, index) {
+    if (start_new_structure(child, prev, index)) return true;
     if (child.name != prev.name) return true;
     //if (child.group_size != prev.group_size) return true;
     if (child.background != prev.background) return true;
@@ -275,22 +276,22 @@ function start_new_block_container(child, prev) {
     return false;
 }
 /*
-function start_new_group_column(child, prev) {
-    //if (start_new_group_row(child, prev)) return true;
+function start_new_group_column(child, prev, index) {
+    //if (start_new_group_row(child, prev, index)) return true;
     return false;
 }
-function start_new_group_row(child, prev) {
-    //if (start_new_group_container(child, prev)) return true;
+function start_new_group_row(child, prev, index) {
+    //if (start_new_group_container(child, prev, index)) return true;
     return false;
 }
-function start_new_group_container(child, prev) {
-    if (start_new_structure(child, prev)) return true;
+function start_new_group_container(child, prev, index) {
+    if (start_new_structure(child, prev, index)) return true;
     //if (child.inVersions != prev.inVersions) return true;
     //if (child.dynamic_content != prev.dynamic_content) return true;
     return false;
 }
 */
-function start_new_structure(child, prev) {
+function start_new_structure(child, prev, index) {
     if (child.dynamicContent?.split("/")[0] != prev.dynamicContent?.split("/")[0]) {
         return true;
     }
